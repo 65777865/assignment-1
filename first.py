8345915b5a31e54f5b2592e6c17eee04f3239d45
65777865 <amnaarif1508@gmail.com>
2024-10-30 15:13:29 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,41 @@
+a=50
+b=20
+c=60
+sum=(a+b+c)
+print(sum) #130
+
+diff1=(a-b)
+print(diff1) #30
+diff2=(c-a)
+print(diff2) #10
+
+product1=(a*b)
+print(product1) #1000
+product2=(b*c)
+print(product2) #1200
+
+average=(sum/3)
+print(average) #43.3
+
+#area of square
+side=(16)
+area1=(side*side)
+print(area1) #256
+
+#area of rectangle
+length=12
+width=9.5
+area2=(length*width)
+print(area2) #114
+
+#area of triangle
+base=5
+height=4
+area3=(1/2*base*height)
+print(area3) #10
+
+#area of circle
+π=3.142
+radius=4
+area4=(π*radius**2)
+print(area4) #50.272
